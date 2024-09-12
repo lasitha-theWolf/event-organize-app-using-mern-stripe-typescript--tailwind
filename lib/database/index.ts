8d7ebdfd336a780f28b06979e15e14c2dc78daae
Lasitha Heenkenda <Lasithaheenkenda23@gmail.com>
2024-09-12 10:23:14 +0530

    Update eslint rules and disable specific rules

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -2,14 +2,22 @@ import mongoose from 'mongoose';
 
 const MONGODB_URL = process.env.MONGODB_URI;
 
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
 // Cache connection
-let cached = (global as any).mongoose || { conn:null, promise:null };
+const globalWithMongoose = global as typeof globalThis & { mongoose: MongooseCache };
+
+globalWithMongoose.mongoose = globalWithMongoose.mongoose || { conn: null, promise: null };
+const cached = globalWithMongoose.mongoose;
 
 export const connectToDatabase = async () => {
     if (cached.conn) {
         return cached.conn;
     }
-    if(!MONGODB_URL){
+    if (!MONGODB_URL) {
         throw new Error('MONGODB_URI is not provided');
     }
 
@@ -17,10 +25,10 @@ export const connectToDatabase = async () => {
     cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {
         dbName: 'evently',
         bufferCommands: false,
-    })
+    });
 
     // Store connection
     cached.conn = await cached.promise;
 
     return cached.conn;
-}
+};
